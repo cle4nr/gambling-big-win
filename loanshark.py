e0e73e927a514c50a43d1dada5ab28a48eb51df2
cle4nr <144906981+cle4nr@users.noreply.github.com>
2023-10-13 12:22:19 +0100

    Add files via upload

---
+++ loanshark.py
@@ -0,0 +1,14 @@
+balance = 0
+print("from a loan shark you can get a loan from between 0 mush coins and 5000 mush coins")
+loansize = int(input("what size loan do you want to take out\n"))
+balance += loansize
+print("your balance is now",balance,"you must pay this back in 7 days or else there will be consequences")
+
+"""
+we need to make it so you can pay back the loan
+and to add interest
+and the loan deadline is in a week
+and a day lasts 3 minutes as of now
+"""
+
+    
